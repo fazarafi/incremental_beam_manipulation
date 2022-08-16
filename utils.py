22b24611aa981d4c6c952c486722aeb5e1bee83d
Faza Thirafi <faza.thirafi@gmail.com>
2022-08-16 17:16:15 +0900

    Add updates on utils

--- utils.py
+++ utils.py
@@ -18,16 +18,42 @@ from tgen.futil import read_das
 from tgen.futil import smart_load_absts
 from regex import Regex, UNICODE, IGNORECASE
 
+import time
+import calendar
+
+from pytorch_transformers import BertTokenizer
+
 START_TOK = '<S>'
 END_TOK = '<E>'
 PAD_TOK = '<>'
 RESULTS_DIR = 'output_files/out-text-dir-v3'
+SUMM_RESULTS_DIR = 'output_files/out-text-dir-summ'
 CONFIGS_DIR = 'new_configs'
 CONFIGS_MODEL_DIR = 'new_configs/model_configs'
 TRAIN_BEAM_SAVE_FORMAT = 'output_files/saved_beams/train_vanilla_{}_{}.pickle'
 TEST_BEAM_SAVE_FORMAT = 'output_files/saved_beams/vanilla_{}.pickle'
 VALIDATION_NOT_TEST= True
 DATASET_WEBNLG=False
+
+
+SUMM_START_TOK = 'BOS'
+SUMM_END_TOK = 'EOS'
+SUMM_PAD_TOK = 'PAD'
+SUMM_CLS_TOK = 'CLS'
+
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+# TODO remove
+model_flags = ['hidden_size', 'ff_size', 'heads', 'emb_size', 'enc_layers', 'enc_hidden_size', 'enc_ff_size',
+               'dec_layers', 'dec_hidden_size', 'dec_ff_size', 'encoder', 'ff_actv', 'use_interval']
 
 
 class fakeDAI:
@@ -182,6 +208,8 @@ def get_multi_reference_training_variables():
     das_mr = []
     texts_mr = []
     for da, text in da_text_map.items():
+        # print("[FT DEBUG]")
+        # print(text[0][0].encode('utf-8'))
         das_mr.append(da)
         texts_mr.append(text)
     return texts_mr, das_mr
@@ -308,3 +336,109 @@ def get_section_value(val, cut_offs, regression_vals, merge_middle=False, only_t
         if val <= co:
             return group_sections(regression_vals[i])
     return group_sections(1)
+
+
+def get_args_presumm(parser):
+    parser.add_argument("-task", default='abs', type=str, choices=['ext', 'abs'])
+    parser.add_argument("-encoder", default='bert', type=str, choices=['bert', 'baseline'])
+    parser.add_argument("-mode", default='train', type=str, choices=['train', 'validate', 'test'])
+    parser.add_argument("-bert_data_path", default='PreSumm/bert_data/bert_data_dummy/xsum') # TODO change
+    parser.add_argument("-model_path", default='PreSumm/models/')
+
+    parser.add_argument("-result_path", default='PreSumm/results/xsum')
+    parser.add_argument("-temp_dir", default='PreSumm/temp')
+
+    parser.add_argument("-batch_size", default=16, type=int)
+    parser.add_argument("-test_batch_size", default=16, type=int)
+
+    parser.add_argument("-max_pos", default=512, type=int)
+    parser.add_argument("-use_interval", type=str2bool, nargs='?',const=True,default=True)
+    parser.add_argument("-large", type=str2bool, nargs='?',const=True,default=False)
+    parser.add_argument("-load_from_extractive", default='', type=str)
+
+    parser.add_argument("-sep_optim", type=str2bool, nargs='?',const=True,default=False)
+    parser.add_argument("-lr_bert", default=2e-3, type=float)
+    parser.add_argument("-lr_dec", default=2e-3, type=float)
+    parser.add_argument("-use_bert_emb", type=str2bool, nargs='?',const=True,default=False)
+
+    parser.add_argument("-share_emb", type=str2bool, nargs='?', const=True, default=False)
+    parser.add_argument("-finetune_bert", type=str2bool, nargs='?', const=True, default=True)
+    parser.add_argument("-dec_dropout", default=0.2, type=float)
+    parser.add_argument("-dec_layers", default=6, type=int)
+    parser.add_argument("-dec_hidden_size", default=768, type=int)
+    parser.add_argument("-dec_heads", default=8, type=int)
+    parser.add_argument("-dec_ff_size", default=2048, type=int)
+    parser.add_argument("-enc_hidden_size", default=512, type=int)
+    parser.add_argument("-enc_ff_size", default=512, type=int)
+    parser.add_argument("-enc_dropout", default=0.2, type=float)
+    parser.add_argument("-enc_layers", default=6, type=int)
+
+    parser.add_argument("-label_smoothing", default=0.1, type=float)
+    parser.add_argument("-generator_shard_size", default=32, type=int)
+    parser.add_argument("-alpha",  default=0.6, type=float)
+    parser.add_argument("-beam_size", default=5, type=int)
+    parser.add_argument("-min_length", default=15, type=int)
+    parser.add_argument("-max_length", default=150, type=int)
+    parser.add_argument("-max_tgt_len", default=140, type=int)
+
+    parser.add_argument("-param_init", default=0, type=float)
+    parser.add_argument("-param_init_glorot", type=str2bool, nargs='?',const=True,default=True)
+    parser.add_argument("-optim", default='adam', type=str)
+    parser.add_argument("-lr", default=1, type=float)
+    parser.add_argument("-beta1", default= 0.9, type=float)
+    parser.add_argument("-beta2", default=0.999, type=float)
+    parser.add_argument("-warmup_steps", default=8000, type=int)
+    parser.add_argument("-warmup_steps_bert", default=8000, type=int)
+    parser.add_argument("-warmup_steps_dec", default=8000, type=int)
+    parser.add_argument("-max_grad_norm", default=0, type=float)
+
+    parser.add_argument("-save_checkpoint_steps", default=5, type=int)
+    parser.add_argument("-accum_count", default=1, type=int)
+    parser.add_argument("-report_every", default=1, type=int)
+    parser.add_argument("-train_steps", default=1000, type=int)
+    parser.add_argument("-recall_eval", type=str2bool, nargs='?',const=True,default=False)
+
+    parser.add_argument('-visible_gpus', default='-1', type=str)
+    parser.add_argument('-gpu_ranks', default='-1', type=str)
+    parser.add_argument('-log_file', default='PreSumm/logs/xsum.log')
+    parser.add_argument('-seed', default=666, type=int)
+    
+    parser.add_argument("-test_all", type=str2bool, nargs='?',const=True,default=False)
+    parser.add_argument("-test_from", default='PreSumm/models/model_step_30000.pt') # TODO FT: for xsum, cnndm change?
+    parser.add_argument("-test_start_from", default=-1, type=int)
+
+    parser.add_argument("-train_from", default='')
+    parser.add_argument("-report_rouge", type=str2bool, nargs='?',const=True,default=True)
+    parser.add_argument("-block_trigram", type=str2bool, nargs='?', const=True, default=True)
+
+    parser.add_argument("-lr_fact", default=0.01, type=float)
+    parser.add_argument("-w1", default=1.00, type=float)
+    parser.add_argument("-w2", default=1.00, type=float)
+    parser.add_argument("-train_weight", default=False, type=float)
+    
+    parser.add_argument("-device", default='', type=str)
+
+    return parser
+
+
+def get_timestamp_file():
+    # gmt stores current gmtime
+    gmt = time.gmtime()
+    ts = calendar.timegm(gmt)
+    
+    return str(ts)
+        
+def convert_id_to_text(tokenizer, token_ids):
+    text = tokenizer.convert_ids_to_tokens([int(n) for n in token_ids])
+    # Convert token_ids to text for factual consistency scoring
+    text = ' '.join(text).replace(' ##','')
+    text = text.replace('[unused0]', '').replace('[unused3]', '').replace('[PAD]', '').replace('[unused1]', '').replace(r' +', ' ').replace(' [unused2] ', '<q>').replace('[unused2]', '').strip()
+    
+    return text
+
+
+# def get_embedding_symbols(token):
+#     symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
+#                'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
+#     return symbols[token]
+            
