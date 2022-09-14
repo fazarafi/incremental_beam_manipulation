bc6b4c7712179584a84800a8857337b4da74fd30
Faza Thirafi <faza.thirafi@gmail.com>
2022-09-14 08:18:46 +0000

    Checkpoint: multi scorers in args

--- _get_results_fact_scores.py
+++ _get_results_fact_scores.py
@@ -7,13 +7,9 @@ import argparse
 from e2e_metrics.metrics.pymteval import BLEUScore
 from e2e_metrics.measure_scores import load_data
 from utils import RESULTS_DIR, VALIDATION_NOT_TEST, DATASET_WEBNLG, \
-    get_args_presumm, convert_id_to_text, SUMM_RESULTS_DIR
+    get_args_presumm, convert_id_to_text, SUMM_RESULTS_DIR, get_timestamp_file
 
 
-from PreSumm.src.models import data_loader, model_builder
-from PreSumm.src.models.data_loader import load_dataset
-# from PreSumm.src.models.model_builder import AbsSummarizer
-# from PreSumm.src.models.predictor import build_predictor
 from pytorch_transformers import BertTokenizer
 import torch
 
@@ -38,8 +34,6 @@ def load_data(pred_file, ref_file):
 
 def test_summary_scores_official(args, pred_file_name, scorers):
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-
     pred_file = os.path.join(SUMM_RESULTS_DIR, pred_file_name)
 
     summaries_sys = []
@@ -58,29 +52,6 @@ def test_summary_scores_official(args, pred_file_name, scorers):
         for line in fin3:
             documents_ref.append(line)
 
-    # for paths in pred_file:
-    #     summaries_sys.append(paths[1])
-
-    # mode = args.use_data
-    
-    # summ_data = None
-    # if mode == 'test':
-    #     summ_data = data_loader.Dataloader(args, load_dataset(args, 'test', shuffle=False), args.batch_size, args.device, shuffle=False, is_test=True)
-    # elif mode == 'valid':
-    #     summ_data = data_loader.Dataloader(args, load_dataset(args, 'valid', shuffle=False), args.batch_size, args.device, shuffle=False, is_test=True)
-    # else:
-    #     summ_data = data_loader.Dataloader(args, load_dataset(args, 'train', shuffle=False), args.batch_size, args.device, shuffle=False, is_test=True)
-
-    # usage_data_for_train = 10000 # TODO FT revise properly
-    # counter = 0
-    # for batch in summ_data:
-    #     if mode == 'train' and counter >= usage_data_for_train:
-    #         break
-    #     for src in batch.src:
-    #         documents_ref.append(convert_id_to_text(tokenizer, src))
-    #     for tgt in batch.tgt:
-    #         summaries_ref.append(convert_id_to_text(tokenizer, src))
-    #     counter += len(batch.src)
     
     final_results = []
     print("len(documents_ref), len(summaries_ref), len(summaries_sys):", len(documents_ref)," ", len(summaries_ref)," ", len(summaries_sys))
@@ -91,6 +62,17 @@ def test_summary_scores_official(args, pred_file_name, scorers):
     summaries_ref = summaries_ref[:data_length]
     summaries_sys = summaries_sys[:data_length]
 
+    if 'rouge' in scorers:
+        print('TEST WITH ROUGE')
+        final_scores = {}
+        final_scores["scorer"] = 'rouge'
+        rouge = Rouge()
+        scores = rouge.get_scores(summaries_sys, summaries_ref, avg=True)
+        final_scores["raw_scores"] = scores
+        
+        print('TEST WITH ROUGE: ', final_scores)
+        final_results.append(final_scores)
+
     if 'factcc' in scorers:
         final_scores = {}
         print('TEST WITH FactCC')
@@ -99,16 +81,18 @@ def test_summary_scores_official(args, pred_file_name, scorers):
         results = factcc_scorer.evaluate_batch(documents_ref, summaries_sys)
         final_scores["raw_scores"] = results
 
+        print('TEST WITH FactCC: ', final_scores)
         final_results.append(final_scores)
 
     if 'summac' in scorers:
         print('TEST WITH SummaC')
         final_scores = {}
         final_scores["scorer"] = 'summac'
-        f1, scores = summac_evaluate_batch(documents_ref, summaries_sys)
-        final_scores["f1_score"] = f1
-        final_scores["raw_scores"] = scores
+        f1, f1_score = summac_evaluate_batch(documents_ref, summaries_sys)
+        final_scores["f1_score"] = f1_score
+        # final_scores["raw_scores"] = f1
         
+        print('TEST WITH SummaC: ', final_scores)
         final_results.append(final_scores)
 
     if 'feqa' in scorers:
@@ -121,151 +105,11 @@ def test_summary_scores_official(args, pred_file_name, scorers):
         # final_scores["average"] = average            
         agg_score = model.compute_score(documents_ref, summaries_sys, aggregate=True)
         final_scores["raw_scores"] = agg_score
-
-        final_results.append(final_scores)
         
-
-    if 'rouge' in scorers:
-        print('TEST WITH ROUGE')
-        final_scores = {}
-        final_scores["scorer"] = 'rouge'
-        rouge = Rouge()
-        scores = rouge.get_scores(summaries_sys, summaries_ref, avg=True)
-        final_scores["raw_scores"] = scores
-        
+        print('TEST WITH FEQA: ', final_scores)
         final_results.append(final_scores)
-
-    return final_results
-
-# TODO FT remove below if above works
-def test_summary_scores(args, pred_file_name, scorer, mode, pred_mode=None):
-
-    use_data_mode = args.use_data
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-
-    pred_file = os.path.join(SUMM_RESULTS_DIR, pred_file_name)
-
-    final_scores = {}
-    final_scores["scorer"] = scorer
     
-    dummy_documents = [
-                "The world's oldest person has died a \
-                few weeks after celebrating her 117th birthday.  \
-                Born on March 5, 1898, the greatgrandmother had lived through two world \
-                wars, the invention of the television and the \
-                first successful powered aeroplane.", 
-                "The world's oldest person has died a \
-                few weeks after celebrating her 117th birthday.  \
-                Born on March 5, 1898, the greatgrandmother had lived through two world \
-                wars, the invention of the television and the \
-                first successful powered aeroplane."]
-    dummy_ref_summaries = [
-                "The world's oldest person died in 1898",
-                "The world's oldest person died after her 117th birthday"]
-
-    dummy_sys_summaries = [
-                "Someone oldest person died",
-                "The world's born after her 117th birthday"]
-    documents_ref = dummy_documents
-    summaries_ref = dummy_ref_summaries
-    summaries_sys = dummy_sys_summaries
-
-    if (scorer=='factcc'):
-        print('TEST WITH FactCC')
-        factcc_scorer = FactccCaller()
-        results = factcc_scorer.evaluate_batch(documents_ref, summaries_sys)
-        final_scores["raw_scores"] = results
-
-    elif (scorer=='summac'):
-        print('TEST WITH SummaC')
-        f1, scores = summac_evaluate_batch(documents_ref, summaries_sys)
-        final_scores["raw_scores"] = scores
-        final_scores["f1_score"] = f1
-
-    elif (scorer=='feqa'):
-        print('TEST WITH FEQA')
-        model = FEQA(use_gpu=True)
-        agg_score = model.compute_score(documents_ref, summaries_sys, aggregate=True)
-        final_scores["raw_scores"] = agg_score
-
-    elif (scorer=='rouge'):
-        print('TEST WITH ROUGE')
-        rouge = Rouge()
-        scores = rouge.get_scores(summaries_sys, summaries_ref, avg=True)
-        final_scores["raw_scores"] = scores
-
-    else:
-        print('Test module is not recognized.')
-
-    return final_scores
-
-
-
-
-def test_res_fact_scores(pred_file_name, scorer, mode):
-    pred_file = os.path.join(RESULTS_DIR, pred_file_name)
-    if DATASET_WEBNLG:
-        if VALIDATION_NOT_TEST:
-            true_file = "WebNLG_Reader/data/webnlg/valid.txt"
-        else:
-            true_file = "WebNLG_Reader/data/webnlg/test.txt"
-    else:
-        if VALIDATION_NOT_TEST:
-            true_file = "tgen/e2e-challenge/input/devel-conc.txt"
-        else:
-            true_file = "tgen/e2e-challenge/input/test-conc.txt"
-
-    _, data_ref, data_sys = load_data(true_file, pred_file)
-
-    bleu = BLEUScore()
-    for sents_ref, sent_sys in zip(data_ref, data_sys):
-        bleu.append(sent_sys, sents_ref)
-
-    total_bleu_score = bleu.score()
-    bleu_scores = []
-    # for sents_ref, sent_sys in zip(data_ref, data_sys):
-    #     bleu.reset()
-    #     bleu.append(sent_sys, sents_ref)
-    #     bleu_scores.append(bleu.score())
-    # # return the computed scores
-    # if total_bleu_score > 0.6:
-    #     print(bleu_scores)
-    #     pass
-
-    return total_bleu_score
-
-def test_res_official(pred_file_name):
-    pred_file = os.path.join(RESULTS_DIR, pred_file_name)
-    if DATASET_WEBNLG:
-        if VALIDATION_NOT_TEST:
-            true_file = "WebNLG_Reader/data/webnlg/valid.txt"
-        else:
-            true_file = "WebNLG_Reader/data/webnlg/test.txt"
-    else:
-        if VALIDATION_NOT_TEST:
-            true_file = "tgen/e2e-challenge/input/devel-conc.txt"
-        else:
-            true_file = "tgen/e2e-challenge/input/test-conc.txt"
-
-    _, data_ref, data_sys = load_data(true_file, pred_file)
-
-    bleu = BLEUScore()
-    for sents_ref, sent_sys in zip(data_ref, data_sys):
-        bleu.append(sent_sys, sents_ref)
-
-    total_bleu_score = bleu.score()
-    bleu_scores = []
-    # for sents_ref, sent_sys in zip(data_ref, data_sys):
-    #     bleu.reset()
-    #     bleu.append(sent_sys, sents_ref)
-    #     bleu_scores.append(bleu.score())
-    # # return the computed scores
-    # if total_bleu_score > 0.6:
-    #     print(bleu_scores)
-    #     pass
-
-    return total_bleu_score
-
+    return final_results
 
 def print_results(args, summ_scorer=None):
     if summ_scorer == None:
@@ -296,12 +140,11 @@ if __name__ == "__main__":
     # SUMM_RESULTS_DIR = 'output_files/from_gpu_2/out-text-dir-v3'
     parser = argparse.ArgumentParser()
 
-
     # PreSumm parser
     parser = get_args_presumm(parser)
 
-    parser.add_argument('-c', default=None)
-    parser.add_argument('-summ_scorer', default='factcc') # TODO multi tests
+    parser.add_argument('-pred_file', default=None)
+    parser.add_argument('-eval_scorers', default=None)
     args = parser.parse_args()
 
 
@@ -314,49 +157,19 @@ if __name__ == "__main__":
     device = "cpu" if args.visible_gpus == '-1' else "cuda"
     device_id = 0 if device == "cuda" else -1
     
-    print_results(args)
+    # print_results(args)
     
+    scorers = args.eval_scorers.split(',')
+
+    pred_file_name = args.pred_file
     
-    for scorer in scorers:
-        result = test_summary_scores_official(args, scorer)    
-        print_results(args, scorer)
-
-
-    result = test_summary_scores_official(args, '', 'factcc', 'test')
+    result = test_summary_scores_official(args, pred_file_name, scorers)    
+    
+    pred_file = os.path.join(SUMM_RESULTS_DIR, pred_file_name)
+    
     print(result)
-    
-    result = test_summary_scores(args, '', 'summac', 'test')
-    print(result)
-    
-    result = test_summary_scores(args, '', 'feqa', 'test')
-    print(result)
-    
-    result = test_summary_scores(args, '', 'rouge', 'test')
-    print(result)
-    
+    print("END")
 
-    
-
-    # scorer = FEQA(use_gpu=True)
-
-    # documents = [
-    #             "The world's oldest person has died a \
-    #             few weeks after celebrating her 117th birthday.  \
-    #             Born on March 5, 1898, the greatgrandmother had lived through two world \
-    #             wars, the invention of the television and the \
-    #             first successful powered aeroplane.", 
-    #             "The world's oldest person has died a \
-    #             few weeks after celebrating her 117th birthday.  \
-    #             Born on March 5, 1898, the greatgrandmother had lived through two world \
-    #             wars, the invention of the television and the \
-    #             first successful powered aeroplane."]
-    # summaries = [
-    #             "The world's oldest person died in 1898",
-    #             "The world's oldest person died after her 117th birthday"]
-    # scores1 = scorer.compute_score(documents, summaries, aggregate=False)
-
-    
-    # scores2 = scorer.compute_score(documents, summaries, aggregate=True)
-
-    # print("[DEBUG FT] non SCORE: " + str(scores1))
-    # print("[DEBUG FT] Aggregate SCORE: " + str(scores2))
+    with open(pred_file + '.test_result_cust_' + get_timestamp_file(), "w+", encoding='utf-8') as out_file_result:
+        for res in result:
+            out_file_result.write(str(res) + '\n')
